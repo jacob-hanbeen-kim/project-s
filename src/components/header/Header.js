9f55c5b4e7063563c66c094d98270d325c5273c6
jacob-hanbeen-kim <kjkjkjhan424@gmail.com>
2022-01-11 20:12:10 -0500

    initial setup react

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+    return (
+        <header className='header'>
+            <h1 >{title}</h1>
+        </header>
+    )
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from './header/Header'
+import Navbar from './navbar/Navbar'
+import Button from './button/Button'
+
+export { Header, Navbar, Button }
