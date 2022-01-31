d6704a182fb8f77b63b1fcf083a3760edc2b9519
sj43 <seunghunjang956@gmail.com>
2022-02-01 00:44:44 +0900

    contract inherits ownable from openzeppelin

--- backend/contracts/SportsContract.sol
+++ backend/contracts/SportsContract.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 // An example of a basic wallet.
 // Anyone can send ETH.
 // Only the owner can withdraw.
-contract SportsContract {
-    address payable public owner;
-
+contract SportsContract is Ownable {
     constructor() {
-        owner = payable(msg.sender);
     }
 
-    receive() external payable {}
-
-    function withdraw(uint _amount) external {
-        require(msg.sender == owner, "caller is not owner");
+    event Received(address, uint);
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+    
+    function withdraw(uint _amount) external onlyOwner {
         payable(msg.sender).transfer(_amount);
     }
 
