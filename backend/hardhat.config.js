5a56c7d9869ee02e33d91a0e9d33f1a67f79e983
sj43 <seunghunjang956@gmail.com>
2022-02-01 00:45:45 +0900

    importing ethers.js and openzeppelin contracts; deploying sportscontract and testing functionality

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, API_KEY, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,6 +21,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+ module.exports = {
   solidity: "0.8.4",
-};
+  defaultNetwork: "ropsten",
+  networks: {
+     hardhat: {},
+     ropsten: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     }
+  },
+}
