f169ec08ac879a779dde3ed6539067b0a61507cf
jacob-hanbeen-kim <kjkjkjhan424@gmail.com>
2022-02-02 14:29:31 -0500

    remove not used nav links and pages

--- src/pages/Brands/Brands.js
+++ src/pages/Brands/Brands.js
@@ -2,34 +2,9 @@ import {
     BrandsContainer,
 } from './Brands.styled';
 
-import { Card, Carousel } from '../../components';
-
 const Brand = () => {
     return (
         <BrandsContainer>
-            <Carousel children={
-                [
-                    <Card
-                        image="images/uniform.jpg"
-                        title="Uniform Logo"
-                        details="add custom logo to our team's uniform"
-                        buttonTxt="Learn More"
-                    />,
-                    <Card
-                        image="images/uniform.jpg"
-                        title="Uniform Logo"
-                        details="add custom logo to our team's uniform"
-                        buttonTxt="Learn More"
-                    />,
-                    <Card
-                        image="images/uniform.jpg"
-                        title="Uniform Logo"
-                        details="add custom logo to our team's uniform"
-                        buttonTxt="Learn More"
-                    />
-                ]
-            }>
-            </Carousel>
         </BrandsContainer>
     )
 }
