c195c886ea3d046753d5d6ef2662e200db80521f
sj43 <seunghunjang956@gmail.com>
2022-03-08 14:48:47 -0800

    deployment script for unidirectional contract

---
+++ backend/scripts/deploy-unidirectionalcontract.js
@@ -0,0 +1,15 @@
+async function main() {
+    const Subscription = await ethers.getContractFactory("UniDirectionalPaymentChannel");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const subscription = await Subscription.deploy();
+    console.log("Contract deployed to address:", subscription.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
